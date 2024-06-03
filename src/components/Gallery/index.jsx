8070318229a1bb0e9114ed43ae6c4d3eb12ca62e
M.can <caglayan.mertcan37@gmail.com>
2024-06-03 19:31:38 +0300

    gallery created

---
+++ src/components/Gallery/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './gallery.css'
+import GalleryCard from "./GalleryCard";
+
+function Gallery() {
+	return (
+		<div className="gallery">
+			<GalleryCard></GalleryCard>
+		</div>
+	);
+}
+
+export default Gallery;
